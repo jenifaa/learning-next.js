3824157d7490ace2710ed5d9f6fbe5bb78054b18
Nahida Akter Jenifa <jefrahman32@gmail.com>
2026-07-14 01:39:13 +0600

    created app

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">About Us</h1>
+      <p>Welcome to our about page!</p>
+    </div>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Contact Us</h1>
+      <p>Get in touch with us!</p>
+    </div>
+  );
+}
