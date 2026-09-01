e88716def7679050424d73265f8da0afdbd5b629
Nahida Akter Jenifa <jefrahman32@gmail.com>
2026-09-01 21:44:27 +0600

    implement force cache

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,9 +2,11 @@ import ProductCards from "@/src/components/Products/ProductCards";
 import { IProduct } from "@/src/type";
 
 const ProductsPage = async () => {
-  const res = await fetch("http://localhost:5000/products");
+  const res = await fetch("http://localhost:5000/products",{
+    cache:"force-cache"
+  });
   const products = await res.json();
-  console.log(products);
+//   console.log(products);
   return (
     <div>
       <h2>Products</h2>
