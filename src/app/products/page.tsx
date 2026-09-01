e01e7b74913fb17bb50dc7567425335e20666afd
Nahida Akter Jenifa <jefrahman32@gmail.com>
2026-09-01 21:52:51 +0600

    use revalidate

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,16 +2,18 @@ import ProductCards from "@/src/components/Products/ProductCards";
 import { IProduct } from "@/src/type";
 
 const ProductsPage = async () => {
-  const res = await fetch("http://localhost:5000/products",{
-    cache:"force-cache"
+  const res = await fetch("http://localhost:5000/products", {
+    next: {
+      revalidate: 10,
+    },
   });
   const products = await res.json();
-//   console.log(products);
+  //   console.log(products);
   return (
     <div>
       <h2>Products</h2>
       <div className="grid grid-cols-3 gap-4">
-        {products.map((product : IProduct) => (
+        {products.map((product: IProduct) => (
           <ProductCards key={product.id} product={product} />
         ))}
       </div>
