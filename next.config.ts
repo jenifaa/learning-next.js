4086e1ac6749ff0bcce77e0a66c3551337f5c8ed
Nahida Akter Jenifa <jefrahman32@gmail.com>
2026-09-01 20:32:29 +0600

    create db and fakedata

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
+  // images: {
+  //   remotePatterns: [new URL("https://i.ibb.co.com/Nn1VPXy5/apple.avif")],
+  // },
   images: {
-    remotePatterns: [new URL("https://i.ibb.co.com/Nn1VPXy5/apple.avif")],
+    remotePatterns: [{
+      protocol:"https",
+      hostname:"**",
+     
+    }],
   },
 };
 
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image"
-import pet from "../../../public/images/pet.webp"
+
 
 function GalleryPage() {
   return (
     <div>
-      <Image src="https://i.ibb.co.com/Nn1VPXy5/apple.avif" width={500} height={300} alt="Pet" />
+      <Image src="/pet.webp" width={500} height={300} alt="Pet" />
     </div>
   )
 }
